d6cb2c802771d30ed3ddd986bd0a177b36fdca62
saman shakya <samanshakya@samans-MacBook-Air.local>
2026-03-16 10:43:59 +0545

    fix env variable for CRA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API = `${import.meta.env.VITE_API_URL || "http://localhost:8000"}/todos`;
+const API = `${process.env.REACT_APP_API_URL || "http://localhost:8000"}/todos`;
 
 function App() {
   const [todos, setTodos] = useState([]);
